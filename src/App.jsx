db113dd5f28d5dc6276c68a307049cd111dd4385
Huggodev <hugojavier02@gmail.com>
2023-11-01 18:32:23 -0300

    Parcial primera parte

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Card'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Carga de Estudiantes</h1>
+        <form action=""></form>
+        <Card/>
+       
+      </div>
+    </>
+  )
+}
+
+export default App
